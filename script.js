a7d5925f147ec3a1a491bf45a0da068fffe863de
BurakUni <21010310070@ogrenci.bartin.edu.tr>
2023-03-14 19:52:30 +0300

    Animated Rating

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', () => {
+    const fr = new FaceRating("#face-rating");
+})
+
+class FaceRating {
+    constructor(qs) {
+        this.input = document.querySelector(qs);
+        this.input?.addEventListener('input',this.update.bind(this));
+        this.face = this.input?.previousElementSibling;
+        this.update();
+    }
+
+    update(e) {
+        let value = this.input.defaultValue;
+
+        if(e) value = e.target?.value;
+        else this.input.value = value
+
+        const min = this.input.min || 0;
+        const max = this.input.max || 100;
+        const percentRaw = (value - min) / (max - min) * 100;
+        const percent = +percentRaw.toFixed(2);
+
+        this.input?.style.setProperty("--percent", `${percent}%`);
+
+        const maxHue = 120;
+        const hueExtend = 30;
+        const hue = Math.round(maxHue * percent /100);
+        let hue2 = hue - hueExtend;
+        if(hue2<0) hue2 += 360;
+
+        const hues = [hue,hue2];
+        hues.forEach((h,i) =>{
+            this.face?.style.setProperty(`--face-hue${i + 1}`,h)
+        })
+
+        this.input?.style.setProperty("--input-hue",hue);
+
+        const duration = 1;
+        const delay = -(duration * 0.99) * percent / 100;
+        const parts = ['right','left','mouth-lower','mouth-upper'];
+
+        parts.forEach(p => {
+            const el = this.face?.querySelector(`[data-${p}]`);
+            el?.style.setProperty(`--delay-${p}`,`${delay}s`);
+        });
+    }
+}
